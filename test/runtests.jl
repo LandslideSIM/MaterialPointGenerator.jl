b1273e0b9c9dc51d67d09f1a16600547cec31bc7
ZenanH <zenan.huo@outlook.com>
2024-12-22 17:14:39 +0000

    😶‍🌫️ Remove embreex test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,4 @@ using Test
 @test !isnothing(MaterialPointGenerator.trimesh[])
 @test !isnothing(MaterialPointGenerator.np[])
 @test !isnothing(MaterialPointGenerator.meshio[])
-@test !isnothing(MaterialPointGenerator.embreex[])
 @test !isnothing(MaterialPointGenerator.voxelize_fn[])
