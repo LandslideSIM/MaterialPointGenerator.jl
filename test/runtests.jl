b48f3ae558105d4c96dab8b912d4c2015835ce44
ZenanH <zenan.huo@outlook.com>
2025-01-06 20:51:56 +0100

    🔧 Add test for embreex function on non-aarch64 architectures

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,4 @@ using Test
 @test !isnothing(MaterialPointGenerator.np[])
 @test !isnothing(MaterialPointGenerator.meshio[])
 @test !isnothing(MaterialPointGenerator.voxelize_fn[])
+@test Sys.ARCH ≠ :aarch64 && !isnothing(MaterialPointGenerator.embreex[])
