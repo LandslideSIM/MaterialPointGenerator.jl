50e3cdfff0f265499179414aab375fc4a85a5aac
ZenanH <zenan.huo@outlook.com>
2025-01-23 16:32:12 +0000

    🙃 Fix the problem in the test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,28 @@
+using CondaPkg
+
+if Sys.ARCH ≠ :aarch64
+    CondaPkg.withenv() do
+        cmd = `python -c "import embreex"`
+        try
+            run(cmd)
+            return nothing
+        catch
+            CondaPkg.add_pip("embreex")
+            return nothing
+        end
+    end
+end
+
 using MaterialPointGenerator
 using Test
+
+MaterialPointGenerator.__init__()
 
 @test !isnothing(MaterialPointGenerator.trimesh[])
 @test !isnothing(MaterialPointGenerator.np[])
 @test !isnothing(MaterialPointGenerator.meshio[])
 @test !isnothing(MaterialPointGenerator.voxelize_fn[])
+
+if Sys.ARCH ≠ :aarch64
+    @test !isnothing(MaterialPointGenerator.embreex[])
+end
