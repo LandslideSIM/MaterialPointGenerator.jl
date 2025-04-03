c64555efd39ee5d79b16af08c2a302c29304ec5f
ZenanH <zenan.huo@outlook.com>
2025-04-03 20:34:24 +0000

    🚣🏻‍♂ Refactor Python module and add `embreex` support

--- src/MaterialPointGenerator.jl
+++ src/MaterialPointGenerator.jl
@@ -13,24 +13,57 @@ module MaterialPointGenerator
 
 using DelimitedFiles, CondaPkg, Gmsh, NearestNeighbors, Printf, PythonCall
 
-const trimesh     = Ref{Py}()
-const voxelize_fn = Ref{Py}()
-const np          = Ref{Py}()
-const meshio      = Ref{Py}()
+const trimesh      = PythonCall.pynew()
+const np           = PythonCall.pynew()
+const MultiPolygon = PythonCall.pynew()
+const Polygon      = PythonCall.pynew()
+const Point        = PythonCall.pynew()
+const mapping      = PythonCall.pynew()
+const unary_union  = PythonCall.pynew()
+const rasterio     = PythonCall.pynew()
+const rasterize    = PythonCall.pynew()
+const pygmsh       = PythonCall.pynew()
+const pyKDTree     = PythonCall.pynew()
+const pytime       = PythonCall.pynew()
+const embreex      = PythonCall.pynew()
 
 function __init__()
     @info "checking environment..."
-    trimesh[]     = pyimport("trimesh")
-    np[]          = pyimport("numpy")
-    meshio[]      = pyimport("meshio")
-    voxelize_fn[] = @pyconst(trimesh[].voxel.creation.voxelize)
+    # import Python modules
+    PythonCall.pycopy!(trimesh , pyimport("trimesh" ))
+    PythonCall.pycopy!(np      , pyimport("numpy"   ))
+    PythonCall.pycopy!(rasterio, pyimport("rasterio"))
+    PythonCall.pycopy!(pygmsh  , pyimport("gmsh"    ))
+    PythonCall.pycopy!(pytime  , pyimport("time"    ))
+    # import their submudules
+    PythonCall.pycopy!(Polygon     , pyimport("shapely.geometry" ).Polygon     )
+    PythonCall.pycopy!(Point       , pyimport("shapely.geometry" ).Point       )
+    PythonCall.pycopy!(mapping     , pyimport("shapely.geometry" ).mapping     )
+    PythonCall.pycopy!(unary_union , pyimport("shapely.ops"      ).unary_union )
+    PythonCall.pycopy!(rasterize   , pyimport("rasterio.features").rasterize   )
+    PythonCall.pycopy!(pyKDTree    , pyimport("scipy.spatial"    ).cKDTree      )
+    PythonCall.pycopy!(MultiPolygon, pyimport("shapely.geometry" ).MultiPolygon)
+    if !Sys.isapple()
+        try 
+            if !haskey(CondaPkg.current_pip_packages(), "embreex")
+                CondaPkg.add_pip("embreex")
+                CondaPkg.resolve()
+            end
+            PythonCall.pycopy!(embreex, pyimport("embreex"))
+            @info "embreex loaded"
+        catch e
+            @warn """embreeX - Python Wrapper for Embree
+            1) cannot find compatible version
+            2) some features will fall back to native Python code
+            """
+        end
+    end
 end
-
-voxelize(mesh, pitch) = voxelize_fn[](mesh, pitch=pitch, edge_factor=1.0)
 
 include(joinpath(@__DIR__, "meshgenerator.jl"))
 include(joinpath(@__DIR__, "polygon.jl"      ))
 include(joinpath(@__DIR__, "polyhedron.jl"   ))
 include(joinpath(@__DIR__, "dem.jl"          ))
 include(joinpath(@__DIR__, "utils.jl"        ))
+
 end
